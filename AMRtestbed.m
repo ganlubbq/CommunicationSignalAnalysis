be582644b38380c21f102bcaa47ee45d037c83fa
ahn <34005957+glingi@users.noreply.github.com>
2018-12-27 09:37:25 +0900

    Update AMRtestbed.m

--- AMRtestbed.m
+++ AMRtestbed.m
@@ -1,6 +1,6 @@
 function varargout = AMRtestbed(varargin)
 
-% Last Modified by GUIDE v2.5 26-Jan-2018 09:30:43
+% Last Modified by GUIDE v2.5 22-Dec-2018 21:40:41
 
 %% List of callback function
 % handles = pushbutton_inputSignal_Callback(hObject, eventdata, handles);    
@@ -407,8 +407,8 @@ end
 %% Note
 %  주 스펙트럼외에 부수적인 스펙트럼이 생긴 경우 IQ 데이터 파형을 살펴보고 
 %  파형이 급격하게 변하는 부분에서 파형의 위 아래가 바뀌었는지 살펴볼 것.  
-%  원인 : overflow
-%  해결 : 신호 전력을 낮춘다. 
+%  Cause : overflow
+%  Solution : Decrease the power of signal 
 
 if isfield(handles,'sigName')
     filename = handles.sigName;
@@ -2089,18 +2089,18 @@ try
 	% 톤 스펙트럼 검출에 실패한 경우
         % from symbol rate est.
 
-            if isFM    
-                adFlag = 'Analog';
-                coarseModType = 'FM';                     
-                handles.coarseSamplesPerSymbol  = 0;
-                handles.coarseNumberOfsymbols   = 0;
-                handles.coarseSymbolRate        = 0;                  
-                coarseToneSpacing = 0;
-                modulationOrder= 0;                
-            else
-                adFlag = 'Digital';
-                coarseModType = 'FSK';  
-            end
+	if isFM    
+	    adFlag = 'Analog';
+	    coarseModType = 'FM';                     
+	    handles.coarseSamplesPerSymbol = 0;
+	    handles.coarseNumberOfsymbols = 0;
+	    handles.coarseSymbolRate = 0;                  
+	    coarseToneSpacing = 0;
+	    modulationOrder= 0;                
+	else
+	    adFlag = 'Digital';
+	    coarseModType = 'FSK';  
+	end
         if ~handles.opMode
             %% plot 
             cla(handles.axes_monitoring,'reset');
@@ -2757,3 +2757,88 @@ if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgr
     set(hObject,'BackgroundColor','white');
 end
 
+
+function y = nanmedian(x,dim)
+% FORMAT: Y = NANMEDIAN(X,DIM)
+% 
+%    Median ignoring NaNs
+%
+%    This function enhances the functionality of NANMEDIAN as distributed
+%    in the MATLAB Statistics Toolbox and is meant as a replacement (hence
+%    the identical name).  
+%
+%    NANMEDIAN(X,DIM) calculates the mean along any dimension of the N-D
+%    array X ignoring NaNs.  If DIM is omitted NANMEDIAN averages along the
+%    first non-singleton dimension of X.
+%
+%    Similar replacements exist for NANMEAN, NANSTD, NANMIN, NANMAX, and
+%    NANSUM which are all part of the NaN-suite.
+%
+%    See also MEDIAN
+
+% -------------------------------------------------------------------------
+%    author:      Jan Glscher
+%    affiliation: Neuroimage Nord, University of Hamburg, Germany
+%    email:       glaescher@uke.uni-hamburg.de
+%    
+%    $Revision: 1.2 $ $Date: 2007/07/30 17:19:19 $
+
+if isempty(x)
+	y = [];
+	return
+end
+
+if nargin < 2
+	dim = min(find(size(x)~=1));
+	if isempty(dim)
+		dim = 1;
+	end
+end
+
+siz  = size(x);
+n    = size(x,dim);
+
+% Permute and reshape so that DIM becomes the row dimension of a 2-D array
+perm = [dim:max(length(size(x)),dim) 1:dim-1];
+x = reshape(permute(x,perm),n,prod(siz)/n);
+
+
+% force NaNs to bottom of each column
+x = sort(x,1);
+
+% identify and replace NaNs
+nans = isnan(x);
+x(isnan(x)) = 0;
+
+% new dimension of x
+[n m] = size(x);
+
+% number of non-NaN element in each column
+s = size(x,1) - sum(nans);
+y = zeros(size(s));
+
+% now calculate median for every element in y
+% (does anybody know a more eefficient way than with a 'for'-loop?)
+for i = 1:length(s)
+	if rem(s(i),2) & s(i) > 0
+		y(i) = x((s(i)+1)/2,i);
+	elseif rem(s(i),2)==0 & s(i) > 0
+		y(i) = (x(s(i)/2,i) + x((s(i)/2)+1,i))/2;
+	end
+end
+
+% Protect against a column of NaNs
+i = find(y==0);
+y(i) = i + nan;
+
+% permute and reshape back
+siz(dim) = 1;
+y = ipermute(reshape(y,siz(perm)),perm);
+
+% $Id: nanmedian.m,v 1.2 2007/07/30 17:19:19 glaescher Exp glaescher $
+
+% --- Executes during object deletion, before destroying properties.
+function fig_AMRtestbed_DeleteFcn(hObject, eventdata, handles)
+% hObject    handle to fig_AMRtestbed (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
